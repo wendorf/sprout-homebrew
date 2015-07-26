793ef307d280f9eb16f3cc093efc3c4f1ddc5e4c
Dan Wendorf <dwendorf@pivotal.io>
2015-07-26 10:06:34 -0700

    Use link command instead of execute

--- recipes/launchctl.rb
+++ recipes/launchctl.rb
@@ -9,44 +9,46 @@ end
 
 node["sprout"]["homebrew"]["launchctl"].each do |package, subcommand|
 
+  plist_filename = "homebrew.mxcl.#{package}.plist"
   installation_path = File.expand_path("/usr/local/opt/#{package}")
-  plist = "homebrew.mxcl.#{package}.plist"
+
+  source_plist_filename = File.join(installation_path, plist_filename)
+  launch_agent_plist_filename = File.join(launch_agents_path, plist_filename)
 
   case subcommand
   when "load"
 
-    execute "start at login" do
-      command "ln -sfv #{File.join(installation_path, plist)} #{launch_agents_path}"
-      user node['sprout']['user']
+    link launch_agent_plist_filename do
+      to source_plist_filename
     end
 
     execute "start now" do
-      command "launchctl load -w #{File.join(launch_agents_path, plist)}"
+      command "launchctl load -w #{launch_agent_plist_filename}"
       user node['sprout']['user']
     end
 
   when "unload"
 
     execute "stop" do
-      command "launchctl unload -w #{File.join(launch_agents_path, plist)}"
+      command "launchctl unload -w #{launch_agent_plist_filename}"
       user node['sprout']['user']
-      only_if "test -L #{File.join(launch_agents_path, plist)}"
+      only_if "test -L #{launch_agent_plist_filename}"
     end
 
-    link File.join(launch_agents_path, plist) do
+    link launch_agent_plist_filename do
       action :delete
-      only_if "test -L #{File.join(launch_agents_path, plist)}"
+      only_if "test -L #{launch_agent_plist_filename}"
     end
 
   when "reload"
 
     execute "stop now" do
-      command "launchctl unload -w #{File.join(launch_agents_path, plist)}"
+      command "launchctl unload -w #{launch_agent_plist_filename}"
       user node['sprout']['user']
     end
 
     execute "start now" do
-      command "launchctl load -w #{File.join(launch_agents_path, plist)}"
+      command "launchctl load -w #{launch_agent_plist_filename}"
       user node['sprout']['user']
     end
 
